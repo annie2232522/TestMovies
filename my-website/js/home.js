946ffc32979c4561d806a051a9ced426331beb16
annie2232522 <108311885+annie2232522@users.noreply.github.com>
2025-04-26 19:04:35 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '7ee3f44e92211fe941b4243a38e99265';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
