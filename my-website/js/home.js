4d23a515d3710a13672d4b278aaf9c79a1d4c1e4
annie2232522 <108311885+annie2232522@users.noreply.github.com>
2025-04-28 08:02:35 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -104,14 +104,12 @@ function updateVideo(episodeNumber = 1) {
   let embedUrl = '';
   if (currentItem.media_type === 'movie') {
     embedUrl = `https://${server}/embed/movie/${currentItem.id}`;
-  } else if {
     embedUrl = `https://${server}/movies/${currentItem.id}`;
     
   else {
     embedUrl = `https://${server}/embed/tv/${currentItem.id}/${currentSeason}/${episodeNumber}`;
   }
   document.getElementById('modal-video').src = embedUrl;
-}
 }
 
 function closeModal() {
